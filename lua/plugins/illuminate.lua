079f7d72b27ba99dd090a4be2b41a8b08fb2985b
xzb <xzb@MacBook-Pro.local>
2024-03-01 14:21:34 +0800

    large file cutoff

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -15,9 +15,7 @@ return {
         },
         delay = 1,
         large_file_cutoff = 2000,
-        large_file_overrides = {
-            providers = { "lsp", "treesitter", "regex" },
-        },
+        large_file_overrides = nil,
         modes_denylist = { "i", "v", "x" },
     },
 }
