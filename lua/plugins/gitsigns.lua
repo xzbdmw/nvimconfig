4dc77fe8eacb492e79ada60cef4e417e292ef369
xzb <xzb@MacBook-Pro.local>
2024-03-12 14:33:04 +0800

    diffview

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -63,7 +63,7 @@ return {
                 -- map("n", "<leader>hb", function()
                 --     gs.blame_line({ full = true })
                 -- end)
-                map("n", "<leader>tb", gs.toggle_current_line_blame)
+                map("n", "<leader>ug", gs.toggle_current_line_blame)
                 -- map("n", "<leader>hd", gs.diffthis)
                 -- map("n", "<leader>hD", function()
                 --     gs.diffthis("~")
@@ -75,7 +75,7 @@ return {
             end,
             auto_attach = true,
             attach_to_untracked = false,
-            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+            current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
             current_line_blame_opts = {
                 virt_text = true,
                 virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
@@ -83,7 +83,8 @@ return {
                 ignore_whitespace = false,
                 virt_text_priority = 100,
             },
-            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+            -- current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+            current_line_blame_formatter = "      <author_time:%Y-%m-%d> - <summary>",
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
