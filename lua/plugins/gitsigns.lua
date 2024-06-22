b80d30823d6a0a9ec2f8ad46b57c065e8cb5d98d
xzb <xzb@MacBook-Pro.local>
2024-06-23 05:24:41 +0800

    (gitsigns) blame format add author msg

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -61,7 +61,7 @@ return {
                 virt_text_priority = 100,
             },
             -- current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-            current_line_blame_formatter = "      <author_time:%Y-%m-%d> - <summary>",
+            current_line_blame_formatter = "    <author> <author_time:%Y-%m-%d> - <summary>",
             sign_priority = 6,
             update_debounce = 100,
             status_formatter = nil, -- Use default
