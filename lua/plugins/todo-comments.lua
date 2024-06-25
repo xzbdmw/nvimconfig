ef0177a17485a3f0ac8c8a2d0b34f9e4f7ee1f3c
xzb <xzb@MacBook-Pro.local>
2024-06-25 10:50:07 +0800

    (todocomment) leader tq

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,7 +1,12 @@
 return {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    enabled = false,
+    keys = { {
+        "<leader>tq",
+        "<cmd>TodoQuickFix<cr>",
+    } },
+    event = "VeryLazy",
+    -- enabled = false,
     opts = {
         signs = true, -- show icons in the signs column
         sign_priority = 8, -- sign priority
