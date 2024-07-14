d64840fe14975242fb71532eae8b01c51d0848be
xzb <2598514867@qq.com>
2024-07-14 22:31:01 +0800

    (noice | refactoring) defer vim.cmd('Noice enable')

--- lua/plugins/refactor.lua
+++ lua/plugins/refactor.lua
@@ -13,7 +13,9 @@ return {
                 api.nvim_create_autocmd("CmdlineLeave", {
                     once = true,
                     callback = function()
-                        vim.cmd("Noice enable")
+                        vim.schedule(function()
+                            vim.cmd("Noice enable")
+                        end)
                     end,
                 })
                 return ":Refactor extract "
@@ -28,7 +30,9 @@ return {
                 api.nvim_create_autocmd("CmdlineLeave", {
                     once = true,
                     callback = function()
-                        vim.cmd("Noice enable")
+                        vim.schedule(function()
+                            vim.cmd("Noice enable")
+                        end)
                     end,
                 })
                 return ":Refactor inline_var"
@@ -43,7 +47,9 @@ return {
                 api.nvim_create_autocmd("CmdlineLeave", {
                     once = true,
                     callback = function()
-                        vim.cmd("Noice enable")
+                        vim.schedule(function()
+                            vim.cmd("Noice enable")
+                        end)
                     end,
                 })
                 return ":Refactor extract_var "
@@ -73,38 +79,6 @@ return {
     },
 
     config = function()
-        require("refactoring").setup({ -- prompt for return type
-        })
-        -- vim.keymap.set("x", "<leader>rf", ":Refactor extract ")
-        -- -- vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
-        --
-        -- vim.keymap.set("x", "<leader>re", ":Refactor extract_var ")
-        --
-        -- vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
-        --
-        -- vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
-        --
-        -- vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
-        -- vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
-        -- -- You can also use below = true here to to change the position of the printf
-        -- -- statement (or set two remaps for either one). This remap must be made in normal mode.
-        -- vim.keymap.set("n", "<leader>rp", function()
-        --     require("refactoring").debug.printf({ below = false })
-        -- end)
-        --
-        -- -- Print var
-        --
-        -- vim.keymap.set({ "x", "n" }, "<leader>vp", function()
-        --     require("refactoring").debug.print_var()
-        --     -- vim.schedule(function()
-        --     --     api.nvim_feedkeys(api.nvim_replace_termcodes("jjj", true, false, true), "n", true)
-        --     -- end)
-        -- end)
-        -- -- Supports both visual and normal mode
-        --
-        -- vim.keymap.set("n", "<leader>cc", function()
-        --     require("refactoring").debug.cleanup({})
-        -- end)
-        -- Supports only normal mode
+        require("refactoring").setup({})
     end,
 }
