b700837a2bab66941f1f4826a6d75f6b1a24212a
xzb <2598514867@qq.com>
2024-07-17 05:10:50 +0800

    (hlslens) add new error situation

--- lua/plugins/hlslen.lua
+++ lua/plugins/hlslen.lua
@@ -9,11 +9,10 @@ return {
             nearest_only = true,
             nearest_float_when = "never",
             override_lens = function(render, posList, nearest, idx, relIdxm, extmark)
-                local c = vim.fn.getcmdline()
-                local is_cmdline = c ~= ""
+                local is_cmdline = vim.fn.getcmdline() ~= ""
                 if is_cmdline then
                     mode = vim.fn.getcmdtype()
-                    cmd = c
+                    cmd = vim.fn.getcmdline()
                 end
 
                 if _G.star_search ~= nil then
@@ -21,22 +20,40 @@ return {
                     _G.star_search = nil
                 end
 
-                local lnum, col = unpack(posList[idx])
+                if cmd == nil or cmd == "" or cmd == " " then
+                    render.clear(true, 0, true)
+                    return
+                end
+
                 local cur_row = unpack(api.nvim_win_get_cursor(0))
+
+                local text = ("%s%s "):format(mode, cmd)
+                if posList == nil then
+                    local chunks = { { " " }, { text, "HlSearchLensNearFail" }, { "[0/0]", "HlSearchLensNearFail" } }
+                    render.clear(true, 0, true)
+                    render.setVirt(0, cur_row - 1, 0, chunks, true)
+                    return
+                end
+
+                local lnum, col = unpack(posList[idx])
                 if (not is_cmdline) and cur_row ~= lnum then
                     return
                 end
                 local cnt = #posList
-                local text = ("%s%s"):format(mode, cmd)
-                local count = ("  [%d/%d]"):format(idx, cnt)
+                local count = ("[%d/%d]"):format(idx, cnt)
                 if not is_cmdline then
                     extmark:clearBuf(0)
                     -- because use cmp tab to select does not update search pattern
-                    text = ("%s%s"):format(mode, vim.fn.getreg("/"))
+                    text = ("%s%s "):format(mode, vim.fn.getreg("/"))
                 else
                     render.clear(true, 0, true)
                 end
-                local chunks = { { " " }, { text, "HlSearchLensNear" }, { count, "HlSearchLensCount" } }
+                local chunks = {
+                    { " " },
+                    { mode, "HlSearchLensNear" },
+                    { cmd, "HlSearchLensNear" },
+                    { count, "HlSearchLensCount" },
+                }
                 vim.defer_fn(function()
                     require("treesitter-context").context_hlslens_force_update(_G.parent_bufnr, _G.parent_winid)
                 end, 10)
