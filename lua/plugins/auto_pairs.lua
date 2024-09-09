884fdda9aa5d93b0a33dc07f345bf9bb23c65ca9
xzb <2598514867@qq.com>
2024-09-10 00:29:21 +0800

    feat(auto_pairs): < > rule

--- lua/plugins/auto_pairs.lua
+++ lua/plugins/auto_pairs.lua
@@ -5,7 +5,8 @@ return {
         break_undo = true,
         fast_wrap = {
             map = "<c-e>",
-            chars = { "{", "[", "(", '"', "'" },
+            chars = { "{", "[", "(", '"', "'", "<" },
+            mirror_chars = { "}", "]", ")", '"', "'", ">" },
             pattern = [=[[%'%"%>%]%)}%,]]=],
             end_key = "e",
             before_key = "h",
@@ -20,23 +21,20 @@ return {
         map_bs = true,
         map_cr = true,
     }, -- this is equalent to setup({}) function
-    config = function(_, opts)
-        require("nvim-autopairs").setup(opts)
-        -- local Rule = require("nvim-autopairs.rule")
-        -- local npairs = require("nvim-autopairs")
+    config = function(_, lazy_opts)
+        require("nvim-autopairs").setup(lazy_opts)
+        local Rule = require("nvim-autopairs.rule")
+        local npairs = require("nvim-autopairs")
 
-        -- 添加自动配对规则使得 <> 成为自动配对的字符
-        -- npairs.add_rules({
-        --     Rule("<", ">", "-vim") -- 这里假设我们不想在 Vim 脚本文件中启用这个规则
-        --         :with_pair(function()
-        --             -- local line = opts.line
-        --             -- local cur_pos = opts.col
-        --             -- -- 检查是否在 <> 内部有其他字符，如果有，则不添加配对
-        --             -- if line:sub(cur_pos - 1, cur_pos) == "<>" then
-        --             --     return false
-        --             -- end
-        --             return true
-        --         end),
-        -- })
+        npairs.add_rules({
+            Rule("<", ">", "rust"):with_pair(function(opts)
+                local line = opts.line
+                local cur_pos = opts.col
+                if line:sub(cur_pos - 1, cur_pos) == "<>" then
+                    return false
+                end
+                return true
+            end),
+        })
     end,
 }
