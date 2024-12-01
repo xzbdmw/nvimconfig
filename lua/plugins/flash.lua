4a924a6e37a46d49b81ad9db07afd6a9af92ffe2
xzb <2598514867@qq.com>
2024-12-01 12:24:30 +0800

    feat(flash): ;,

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -9,6 +9,15 @@ return {
             "s",
             mode = { "n", "x", "o" },
             function()
+                local key_ns = vim.api.nvim_create_namespace("flash;")
+                vim.on_key(function(key, typed)
+                    if typed == ";" then
+                        require("config.utils").jump_to_next_match(false)
+                    elseif typed == "," then
+                        require("config.utils").jump_to_prev_match(false)
+                    end
+                end, key_ns)
+                vim.g.disable_arrow = true
                 vim.o.scrolloff = 0
                 vim.g.flash_winbar = vim.wo.winbar
                 vim.cmd([[normal! m']])
